b10e56bb1a35c9bb77b3c4f0d115fa1a496315e8
CodeNameJacks <jmedley60@hotmail.com>
2024-11-25 21:20:51 -0500

    Created Hamburger menu component

--- frontend/src/components/HamburgerNav.jsx
+++ frontend/src/components/HamburgerNav.jsx
@@ -1,5 +1,7 @@
 import Hamburger from 'hamburger-react'
-import React, {useState} from 'react'
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import {i18n} from 'react'
 require("../assets/scss/hamburgerNav.scss");
 
 
@@ -9,11 +11,14 @@ function HamburgerNav() {
     
     return (
       <div>
-        <button> Login</button>
-        <Hamburger toggled={isOpen} toggle={setOpen}></Hamburger>
-        
+        <div className="navContainer">
+          <Hamburger className='burgernav' toggled={isOpen} toggle={setOpen}></Hamburger>
+          <p> Welcome Home </p>
+          <Button variant="light"> Login</Button>
+        </div>
         {isOpen && (
           <> 
+        
             <ul className = 'navlist'>
               <li><a href="/">Home</a></li>
               <li><a href="/findTenant">Tenant Search</a></li>
